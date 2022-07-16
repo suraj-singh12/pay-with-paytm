64745587b3f02eac1625e39b12ca7d2f479e0508
suraj-singh12 <surajsingh5092@gmail.com>
2022-07-16 16:58:32 +0530

    updating redirection url after payment (redirect to my live app on netlify)

--- index.js
+++ index.js
@@ -129,7 +129,10 @@ app.post("/callback", (req, res) => {
           // res.redirect(`https://pay-with-paytm.herokuapp.com/viewBooking?data=${response}`);  // applicable when app runs live
 
           // redirect to my react app running at localhost:3000
-          res.redirect(`http://localhost:3000/viewBooking?status=${_results.STATUS}&ORDERID=${_results.ORDERID}&date=${_results.TXNDATE}&bank=${_results.BANKNAME}`);
+          // res.redirect(`http://localhost:3000/viewBooking?status=${_results.STATUS}&ORDERID=${_results.ORDERID}&date=${_results.TXNDATE}&bank=${_results.BANKNAME}`);
+
+          // redirect to my netlify (live app)
+          res.redirect(`https://zomato-app12.netlify.app/viewBooking?status=${_results.STATUS}&ORDERID=${_results.ORDERID}&date=${_results.TXNDATE}&bank=${_results.BANKNAME}`);
            });
        });
        
