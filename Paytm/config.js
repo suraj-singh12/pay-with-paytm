b9d52dad8fa5c61ef29ddc74a2b1b6f261eae923
suraj-singh12 <surajsingh5092@gmail.com>
2022-07-17 18:49:58 +0530

    udpated config files with own api keys(and removed sir's api keys)

--- Paytm/config.js
+++ Paytm/config.js
@@ -1,6 +1,6 @@
 var PaytmConfig = {
-  mid: "tmRoBh90182073607271",    // Test Merchant ID (tmRoBh90182073607271)  BfXSEL99752579173271
-  key: "fAPNNg3!h3K60ZsM",        // Test Merchant Key (fAPNNg3!h3K60ZsM)   5#_5GcuCpHsNSRRu
+  mid: "YjyPSF45602447857016",    // Test Merchant ID (tmRoBh90182073607271)  BfXSEL99752579173271
+  key: "_rlkmfFOIK4lGP6#",        // Test Merchant Key (fAPNNg3!h3K60ZsM)   5#_5GcuCpHsNSRRu
   website: "WEBSTAGING",          // website
 };
 module.exports.PaytmConfig = PaytmConfig;
